c15d300c00f995ffe15caf06eb31566843fef703
Gayathri-Gondi <92205010+Gayathri-Gondi@users.noreply.github.com>
2024-10-01 01:13:29 +0530

    first commit
    added navbar and logo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>AniVault Home</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" type="text/css" href='lmcss/index.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+    <nav class="nav">
+        <div class="leftcontainer">
+            <img src="images/lmlogo.png" class="logoimg" href="index.php">
+        </div>
+    </nav>
+</body>
+
+</html>
